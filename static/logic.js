823d5c9be59e02355e2f180098389f087d29152f
vinceliu21 <vinceliu21@gmail.com>
2020-03-21 16:47:19 -0400

    removed unused assets

--- static/logic.js
+++ static/logic.js
@@ -5,23 +5,11 @@ let mainSections = document.querySelectorAll("main section");
 let lastId;
 let cur = [];
 
-// This should probably be throttled.
-// Especially because it triggers during smooth scrolling.
-// https://lodash.com/docs/4.17.10#throttle
-// You could do like...
-// window.addEventListener("scroll", () => {
-//    _.throttle(doThatStuff, 100);
-// });
-// Only not doing it here to keep this Pen dependency-free.
 
 window.addEventListener("scroll", event => {
   let fromTop = window.scrollY;
   mainNavLinks.forEach(link => {
     let section = document.querySelector(link.hash);
-    // console.log(section);
-    // console.log(section.offsetTop);
-    // console.log(fromTop);
-    // console.log(section.offsetHeight);
     if (
       section.offsetTop <= fromTop + 100 &&
       section.offsetTop + section.offsetHeight > fromTop
